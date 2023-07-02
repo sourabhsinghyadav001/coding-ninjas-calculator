3fdebcd581f94253383fde5827efe35209244ff0
Sourabh Yadav <sourabh.yadav@objectedge.com>
2023-07-02 20:28:33 +0530

    Finish the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import { buttons } from "./buttons";
 function App() {
+  const [input, setInput] = useState("");
+
+  const evaluate = () => {
+    try {
+      const output = eval(`() => ${input}`)();
+      return output;
+    } catch (err) {
+      if (input === "") return "";
+      else return "Invalid Input";
+    }
+  };
+  const toggle = () => {
+    setInput((prevInput) => {
+      if (prevInput === "") return "";
+      let lastIndex = Math.max(
+        prevInput.lastIndexOf("+"),
+        prevInput.lastIndexOf("-")
+      );
+      const newInput = prevInput.slice();
+      if (lastIndex !== -1)
+        return (
+          newInput.substring(0, lastIndex) +
+          (newInput[lastIndex] === "+" ? "-" : "+") +
+          newInput.substring(lastIndex + 1)
+        );
+      else return prevInput;
+    });
+  };
+  const erase = () => {
+    setInput((prevInput) => prevInput.slice(0, -1));
+  };
+  const appendCharacter = (character) =>
+    setInput((prevInput) => prevInput + character);
+  if (input === undefined) setInput("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="output">{evaluate()}</div>
+      <div className="input">{input}</div>
+      <div className="buttons">
+        <div className="button" onClick={() => setInput("")}>
+          C
+        </div>
+        <div className="button" onClick={() => toggle()}>
+          +/-
+        </div>
+        {buttons.map((buttonContent) => (
+          <div
+            key={buttonContent}
+            onClick={() => appendCharacter(buttonContent)}
+            className="button"
+          >
+            {buttonContent}
+          </div>
+        ))}
+        <div className="button erase" onClick={() => erase()}>
+          erase
+        </div>
+      </div>
     </div>
   );
 }
